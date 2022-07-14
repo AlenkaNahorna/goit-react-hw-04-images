9c58a5e28878036b153170439646a3817d9aed8b
AlenkaNahorna <agyloma@gmail.com>
2022-07-14 21:00:26 +0200

    added library

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        fontSize: 32,
+        color: '#dd1919',
+        textAlign: 'center',
+      }}
+    >
+      goit-react-hw-03-image-finder
+      <div>please wait it is loading</div>
+    </div>
+  );
+};
